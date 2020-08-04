10e367b10f13688d76316900187a2c3656427acc
Anuradha patil <66302519+anuradha50@users.noreply.github.com>
2020-08-04 13:41:52 +0530

    Add files via upload

---
+++ GUIWindow.vb
@@ -0,0 +1,10 @@
+# import all functions from the tkinter
+from tkinter import *
+# Create a GUI window
+root = Tk()
+# Create a global variables
+variable1 =StringVar(root)
+variable2 =StringVar(root)
+# initialise the variables
+variable1.set("currency")
+variable2.set("currency")
